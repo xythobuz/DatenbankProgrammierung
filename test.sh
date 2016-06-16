850ad1ccaa533c65bbbc24a9982b6e8082642b93
Thomas Buck <xythobuz@xythobuz.de>
2016-06-16 23:50:31 +0200

    Test redirects outputs to files

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-./main.py quiet test initialize &
-./main.py quiet test
+rm -rf test1_stdout.txt test1_stderr.txt
+./main.py quiet test initialize > test1_stdout.txt 2> test1_stderr.txt &
+
+rm -rf test2_stdout.txt test2_stderr.txt
+./main.py quiet test > test2_stdout.txt 2> test2_stderr.txt
 
